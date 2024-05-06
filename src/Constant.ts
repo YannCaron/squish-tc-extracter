f9f61a1362abbea74abd00f0c5a0ea7f622c40a6
Yann Caron <cyann74@gmail.com>
2024-05-06 15:47:26 +0200

    constants for node project creation

---
+++ src/Constant.ts
@@ -0,0 +1,23 @@
+export default class Constant {
+
+    static srcRoot = "C:\\projects\\ravens\\poc\\squish\\automatedtests"
+    static destRoot = './generated'
+
+    static nodeConfig = `
+{
+    "name": "generated",
+    "version": "1.0.0",
+    "description": "",
+    "main": "./tst_RWY-010\ -\ Runway\ in\ use\ -\ Modifiable\ -\ GVA\ -\ INCH-4704/test.js",
+    "type": "module",
+    "directories": {
+        "lib": "lib"
+    },
+    "scripts": {
+    },
+    "author": "",
+    "license": "ISC"
+}
+`
+
+}
